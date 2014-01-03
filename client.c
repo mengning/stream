1ae95abac61d3dddc12c717aab7a2c9efc29e008
mengning <mengning@ustc.edu.cn>
2014-01-03 16:06:53 +0800

    重写client和server的测试验证代码 #4 by USTC-KIRA
    https://github.com/mengning/stream/pull/4

--- client.c
+++ client.c
@@ -1,47 +1,75 @@
+/********************************************************************/
+/* Copyright (C) SSE-USTC, 2013-2014                                */
+/*                                                                  */
+/*  FILE NAME             :  client.c                               */
+/*  PRINCIPAL AUTHOR      :  Mengning                               */
+/*  LANGUAGE              :  C                                      */
+/*  TARGET ENVIRONMENT    :  ANY                                    */
+/*  DATE OF FIRST RELEASE :  2014/01/02                             */
+/*  DESCRIPTION           :  test file for stream                   */
+/********************************************************************/
 
-#include<stdio.h> 			/* perror */
-#include<stdlib.h>			/* exit	*/
-#include<sys/types.h>		/* WNOHANG */
-#include<sys/wait.h>		/* waitpid */
-#include<string.h>			/* memset */
+/*
+ * Revision log:
+ *
+ * Created by Lin zenan,2014/01/02
+ *
+ */
+#include<stdio.h> 			
+#include<stdlib.h>			
+#include<sys/types.h>		
+#include<sys/wait.h>		
+#include<string.h>			
 #include<pthread.h>
-
+#include <unistd.h>
 #include "stream.h"
 
-int main() 
-{	
-	unsigned int snd;
-	int error;
-	int erro;
-	struct stream *streamp;
-	uint8_t dscp = 1;
-        /*size_t n = 28;*/
-	const char *buffer = "Hello,this is client,thanks!";
-	size_t n = sizeof(*buffer);
-	const char* name = "tcp:127.0.0.1:1234";	
-	error = stream_open(name,&streamp,dscp);
-	if(error != 0)printf("stream open failure!\n");
-	if(error == 0)
-	{
 
-		erro = stream_connect(streamp);
-		if(erro != 0 ){
-			printf("stream connect failure!\n");
-			stream_close(streamp);
-			return 0;
-		}
-		printf("connect success!\n");
-		snd = stream_send(streamp,buffer,n);
-		if(snd < 1 && snd > n){
-			printf("snd failure!\n");
-			stream_close(streamp);
-		}
-		
-		printf("snd success!\n");			
-		printf("%d\n",snd);
-		printf("%s\n",buffer);
-	}
-	
-	stream_close(streamp);
-	return 0;
+
+int main()
+{
+    struct stream* p_stream;
+    const char* stream_name = "tcp:127.0.0.1:1234";
+    char pnum[4];
+    uint8_t dscp = 1;
+    int actualsend = 0;
+    int index = 0;
+  
+    while(1)
+    {
+        if(stream_open(stream_name,&p_stream,dscp))
+        {
+            printf("stream open failure!\n");
+        }
+        else
+        {
+            printf("stream open sucessed!\n");
+
+            if(stream_connect(p_stream))
+            {
+                printf("stream connect failure!\n");
+            }
+            else
+            {
+                printf("stream connect successed!\n");
+         
+                sprintf(pnum,"%d",index);
+                actualsend = stream_send(p_stream,pnum,sizeof(pnum));
+                index++;
+
+                if(actualsend < 0)
+                    printf("stream send failure!\n");
+                else if(actualsend == 0)
+                    printf("stream send 0 bytes!\n");
+                else
+                    printf("stream actual send %d bytes,Data:%s\n",actualsend,pnum);
+            }
+
+        }
+
+        stream_close(p_stream);
+        sleep(1);
+    }
+
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,55 +1,77 @@
+/********************************************************************/
+/* Copyright (C) SSE-USTC, 2013-2014                                */
+/*                                                                  */
+/*  FILE NAME             :  server.c                               */
+/*  PRINCIPAL AUTHOR      :  Mengning                               */
+/*  LANGUAGE              :  C                                      */
+/*  TARGET ENVIRONMENT    :  ANY                                    */
+/*  DATE OF FIRST RELEASE :  2014/01/02                             */
+/*  DESCRIPTION           :  test file for stream                   */
+/********************************************************************/
 
-#include<stdio.h> 			/* perror */
-#include<stdlib.h>			/* exit	*/
-#include<sys/types.h>		/* WNOHANG */
-#include<sys/wait.h>		/* waitpid */
-#include<string.h>			/* memset */
+/*
+ * Revision log:
+ *
+ * Created by Lin zenan,2014/01/02
+ *
+ */
+
+#include<stdio.h> 			
+#include<stdlib.h>			
+#include<sys/types.h>		
+#include<sys/wait.h>		
+#include<string.h>			
 #include<pthread.h>
-
 #include "stream.h"
 
-int main() 
+
+
+int main()
 {
 
-	unsigned int rec;
-	int error,erro;
-	struct pstream *pstreamp;
-	struct stream *streamp;
-		
-	size_t n = 1024;
-	uint8_t dscp = 1;
-	
-	char *buffer = (char *)malloc(sizeof(char)*n);
-	memset(buffer,0,sizeof *buffer);	
-	const char* name = "ptcp:1234";
-	error = pstream_open(name,&pstreamp,dscp);
-	if(error != 0)printf("pstream open failure!\n");
-	if(error == 0)
-	{	
-		do{
-			erro = pstream_accept(pstreamp,&streamp);
-			/*if(erro != 0 ){
-				printf("pstream accept failure!\n");
-				return 0;
-			}*/
-		}while(erro);
-		if(erro == 0){
-			printf("connect success!\r\n");			
-		}	
-		rec = stream_recv(streamp,buffer,n);
-		if(rec < 1 && rec > n ){
-			printf("recv failure!\n");
-			stream_close(streamp);
-			pstream_close(pstreamp);
-			return 0;
-		}
-		
-		printf("recv success!\n");
-		printf("%d\n",rec);
-		printf("%s\n",buffer);		
-	}
-		
-	stream_close(streamp);
-	pstream_close(pstreamp);
-	return 0;
+    struct pstream* p_pstream; 
+    struct stream* p_stream;
+    const char* pstream_name = "ptcp:1234";
+    char buffer[50];
+    uint8_t dscp = 1;
+    int actualrecv = 0;
+
+
+    if(pstream_open(pstream_name,&p_pstream,dscp))
+        printf("pstream open failure!\n");
+    else
+    {
+        while(1)
+        {  
+    
+	    while(pstream_accept(p_pstream, &p_stream))
+                ;
+            printf("\nget one steam connect success!\n");
+
+            printf("p_pstream name:%s\n", pstream_get_name(p_pstream));
+            printf("p_pstream bound_port:%d\n", pstream_get_bound_port(p_pstream));
+	    printf("p_stream name:%s\n", stream_get_name(p_stream));
+
+
+            printf("<remote_ip,remote_port> : <%d,%d>\n", stream_get_remote_ip(p_stream),stream_get_remote_port(p_stream));
+            printf("<local_ip,local_port> : <%d,%d>\n", stream_get_local_ip(p_stream),stream_get_local_port(p_stream));
+
+            actualrecv = stream_recv(p_stream,buffer,sizeof(buffer));
+ 
+           if(actualrecv < 0)
+              printf("pstream receive failure!\n");
+           else if(actualrecv == 0)
+               printf("pstream receive 0 bytes!\n");
+           else
+               printf("pstream actual receive %d bytes,buffer:%s\n\n",actualrecv,buffer);
+
+         }
+
+
+        stream_close(p_stream);
+        pstream_close(p_pstream);
+
+    }   
+ 
+    return 0;
 }
